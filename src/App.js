3379c2d187d1c7ce9fafc730c1a956d584af5256
ALINE VICTORIA MORAES DE SOUZA <CG3033228@car.ifsp>
2025-05-12 16:57:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
-function App() {
+//ao carregar a aplicação, será executada esta função
+export default () => {
+  const [movieList,setMovieList] = useState([]);
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list); 
+    };
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <section className='Lists'>
+        {movieList.map((item,key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        
+        ))}
+      </section>
     </div>
-  );
-}
-
-export default App;
+  )
+};
