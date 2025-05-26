98b12996e5c32a909338ea86bd533f59ad048297
avms2002 <aline.victoria@aluno.ifsp.edu.br>
2025-05-26 17:21:53 -0300

    Update MovieRow.js

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -6,15 +6,18 @@ export default ({title,items}) => {
         <div>
           <h2>{title}</h2>
           <div className='movieRow-listarea'>
+            <div className="movieRow-list">
             {items.results.length > 0 && 
               items.results.map((item, key) => (
+                <div key={key} className='movieRow-item'>
                 <img 
                 key={item.id}
                   src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} 
                   alt={item.original_title} 
                 />
-              ))
-            }
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       );
