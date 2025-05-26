bb64ced3cb070e6d0d5e0a39128640c01108bbb3
avms2002 <aline.victoria@aluno.ifsp.edu.br>
2025-05-26 17:18:42 -0300

    Update Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -53,5 +53,30 @@ export default {
           
       ];
     },
+
+    //pegaros detalhes do filme
+    getMovieInfo:async (movieId,type) => {
+      let info = {}
+      if (movieId) {
+        switch (type) {
+          case 'movie':
+            info = await basicFetch(
+              `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            );
+            break;
+      
+          case 'tv':
+            info = await basicFetch(
+              `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            );
+            break;
+      
+          default:
+            info = null;
+            break;
+        }
+      }
+      return info; 
+    }
   };
   
